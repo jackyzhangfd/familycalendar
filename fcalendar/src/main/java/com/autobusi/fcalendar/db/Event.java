f075d2c624f7be61a923c15134af347db1da78af
Jacky <Jacky@Jacky-pc>
2017-06-27 21:56:38 +0800

    Improve Calendar UI

--- fcalendar/src/main/java/com/autobusi/fcalendar/db/Event.java
+++ fcalendar/src/main/java/com/autobusi/fcalendar/db/Event.java
@@ -33,5 +33,45 @@ public class Event {
 	public void setName(String name) {
 		this.name = name;
 	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Date getStartTime() {
+		return startTime;
+	}
+
+	public void setStartTime(Date startTime) {
+		this.startTime = startTime;
+	}
+
+	public Date getEndTime() {
+		return endTime;
+	}
+
+	public void setEndTime(Date endTime) {
+		this.endTime = endTime;
+	}
+
+	public String getDetail() {
+		return detail;
+	}
+
+	public void setDetail(String detail) {
+		this.detail = detail;
+	}
+
+	public EventSeries getEventSeries() {
+		return eventSeries;
+	}
+
+	public void setEventSeries(EventSeries eventSeries) {
+		this.eventSeries = eventSeries;
+	}
 	
 }
