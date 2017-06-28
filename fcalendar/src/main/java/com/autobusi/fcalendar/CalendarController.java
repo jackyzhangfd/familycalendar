4ca0f78b5d52f4558de89cc5934ef081b047a84f
I042102 <I042102@PVGN50917286A.apj.global.corp.sap>
2017-06-28 18:17:54 +0800

    Lunar Festival - not finish

--- fcalendar/src/main/java/com/autobusi/fcalendar/CalendarController.java
+++ fcalendar/src/main/java/com/autobusi/fcalendar/CalendarController.java
@@ -1,7 +1,13 @@
 package com.autobusi.fcalendar;
 
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -10,5 +16,15 @@ public class CalendarController {
 	@RequestMapping(value="/")
 	public String index(Model model) {
 		return "index";
+	}
+	
+	@RequestMapping(value="/lunarinfo", headers = "Content-Type=application/json")
+	public Map lunnarInfoOfDate(@RequestBody String date){
+		System.out.println(date);
+	
+		Map lunarInfo = new HashMap<String, Object>();
+		lunarInfo.put("isFestival", true);
+		lunarInfo.put("festivalName", "Lunar Test Festival");
+		return lunarInfo;
 	}
 }
