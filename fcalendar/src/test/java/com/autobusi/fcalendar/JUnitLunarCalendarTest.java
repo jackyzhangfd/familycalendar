baa16d4b729999cbb54aeaa7b345de821e3e05e8
Jackyzhang <Jackyzhang@DESKTOP-VF6FNHL>
2017-07-09 17:15:42 +0800

    keep on fine tune lunar calendar

---
+++ fcalendar/src/test/java/com/autobusi/fcalendar/JUnitLunarCalendarTest.java
@@ -0,0 +1,30 @@
+package com.autobusi.fcalendar;
+
+import static org.junit.Assert.*;
+
+import java.util.Calendar;
+import java.util.Date;
+
+import org.junit.Test;
+
+import com.autobusi.fcalendar.util.LunarCalendar;
+
+public class JUnitLunarCalendarTest {
+
+	@Test
+	public void testGetChinaDayString() {
+		Calendar cal = Calendar.getInstance();
+		for(int i = 1; i<=31; i++){
+			cal.set(Calendar.YEAR, 2049);
+			cal.set(Calendar.MONTH, Calendar.JANUARY);
+			cal.set(Calendar.DATE, i);
+			LunarCalendar lc = new LunarCalendar(cal);
+			System.out.println(lc.getChinaDayString());
+			System.out.println(lc.animalsYear());
+			System.out.println(lc.cyclical());
+			System.out.println(lc.toString());
+		}
+		
+	}
+
+}
